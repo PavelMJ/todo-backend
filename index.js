e39b2873c3c3ae3a573a4e5328337df4baa6bcef
PavelMJ <pavelmat@hotmail.com>
2023-06-04 00:50:06 +0300

    update improved

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ import express from 'express'
 import fs from 'fs'
 import cors from 'cors'
 
-const app =express()
+const app = express()
 app.use(express.static('../todoapp/build'))
 
 app.use(express.json())
 
 app.use(cors())
 
-app.post('/db/register', (req,res)=>{
+app.post('/db/register', (req, res) => {
 	const userData = req.body
 
-	fs.readFile('localDB/users.json', 'utf8', (err, data)=>{
-		if(err){res.status(500).send('read file error')
-				return
-			}
+	fs.readFile('localDB/users.json', 'utf8', (err, data) => {
+		if (err) {
+			res.status(500).send('read file error')
+			return
+		}
 		let userList = JSON.parse(data)
 		userList.push(userData)
-		fs.writeFile('localDB/users.json', JSON.stringify(userList), (err)=>{
-			if(err){
+		fs.writeFile('localDB/users.json', JSON.stringify(userList), (err) => {
+			if (err) {
 				console.error(err)
 				res.status(500).send('')
 			}
@@ -29,42 +30,41 @@ app.post('/db/register', (req,res)=>{
 	})
 })
 
-app.post('/db/login', (req,res)=>{
+app.post('/db/login', (req, res) => {
 	const userData = {
 		userName: req.body.userName,
 		password: req.body.password
 	}
 
-	fs.readFile('localDB/users.json', 'utf-8', (err, data)=>{
-		if(err){
+	fs.readFile('localDB/users.json', 'utf-8', (err, data) => {
+		if (err) {
 			res.status(500).send('read file error')
 		}
 		let match = false
-		const allUsers =JSON.parse(data)
-		console.log(userData)
-		allUsers.forEach((val)=>{
-			if(val.userName === userData.userName && val.password === userData.password){
+		const allUsers = JSON.parse(data)
+		allUsers.forEach((val) => {
+			if (val.userName === userData.userName && val.password === userData.password) {
 				match = true
 			}
 		})
-		if(match===true){
-			fs.readFile('localDB/taskLists.json', 'utf-8', (err, data)=>{
-				if(err){
+		if (match === true) {
+			fs.readFile('localDB/taskLists.json', 'utf-8', (err, data) => {
+				if (err) {
 					res.status(500).send('read file error')
 				}
-				const allLists =JSON.parse(data)
+				const allLists = JSON.parse(data)
 				console.log(allLists)
-				const taskLists = allLists.find(val=> val.userName === userData.userName)
+				const taskLists = allLists.find(val => val.userName === userData.userName)
 				res.json({
-					success:true,
+					success: true,
 					message: 'User not found',
 					taskLists,
 				})
-			})	
+			})
 		}
-		else{
+		else {
 			res.json({
-				success:false,
+				success: false,
 				message: 'User not found'
 			})
 		}
@@ -72,16 +72,15 @@ app.post('/db/login', (req,res)=>{
 	})
 })
 
-app.get('/db/:user', (req, res)=>{
+app.get('/db/:user', (req, res) => {
 	const username = req.params.user;
-	fs.readFile('localDB/taskLists.json', 'utf8', (err, data)=>{
-		if(err){res.status(500).send('read file error')}
-		else{
+	fs.readFile('localDB/taskLists.json', 'utf8', (err, data) => {
+		if (err) { res.status(500).send('read file error') }
+		else {
 			try {
 				const listsArr = JSON.parse(data)
-				const findList = listsArr.find(val=> val.userName === username)
+				const findList = listsArr.find(val => val.userName === username)
 				res.json(findList)
-				
 			} catch (error) {
 				res.status(500).send("parsing error")
 			}
@@ -91,58 +90,44 @@ app.get('/db/:user', (req, res)=>{
 
 
 
-app.post('/db/update', (req,res)=>{
+app.post('/db/update', (req, res) => {
 	const userData = req.body
 	console.log(userData)
-	fs.readFile('localDB/taskLists.json', 'utf-8', (err, data)=>{
-		if(err){
+	fs.readFile('localDB/taskLists.json', 'utf-8', (err, data) => {
+		if (err) {
 			console.error(err)
 			res.status(500).send('read file error')
 		}
 		let match = false
 		let taskList = JSON.parse(data)
-		const newList= taskList.map((val)=>{
-			if(val.userName === userData.userName){
-				val=userData
+		const newList = taskList.map((val) => {
+			if (val.userName === userData.userName) {
+				val = userData
 				match = true
-				 match = true
 			}
 			return val
 		})
-		if(match===false){
+		if (match === false) {
 			taskList.push(userData)
-			fs.writeFile('localDB/taskLists.json', JSON.stringify(taskList), (err)=>{
-				if(err){
+			fs.writeFile('localDB/taskLists.json', JSON.stringify(taskList), (err) => {
+				if (err) {
 					console.error(err)
 					res.status(500).send('parse error')
 				}
 				res.json({
-					success:true,
-					message: 'db updated'
-				})
-		})
-		if(match===false){
-			taskList.push(userData)
-			fs.writeFile('localDB/taskLists.json', JSON.stringify(taskList), (err)=>{
-				if(err){
-					console.error(err)
-					res.status(500).send('parse error')
-				}
-				res.json({
-					success:true,
+					success: true,
 					message: 'db updated'
 				})
 			})
 		}
-
-		else{
-			fs.writeFile('localDB/taskLists.json', JSON.stringify(newList), (err)=>{
-				if(err){
+		else {
+			fs.writeFile('localDB/taskLists.json', JSON.stringify(newList), (err) => {
+				if (err) {
 					console.error(err)
 					res.status(500).send('parse error')
 				}
 				res.json({
-					success:true,
+					success: true,
 					message: 'db updated'
 				})
 			})
@@ -154,8 +139,8 @@ app.post('/db/update', (req,res)=>{
 
 
 
-app.listen('4001',(err)=>{
-	if(err){
+app.listen('4001', (err) => {
+	if (err) {
 		console.log("Server error", err);
 	}
 	console.log('Server running on port 4001');
